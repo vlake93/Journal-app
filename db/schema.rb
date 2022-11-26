f5107c75a78f7c5a198aa1cea506e6c887f3a588
Vlake <apulivic@gmail.com>
2022-11-26 12:53:36 +0800

    added category model and added tester

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_26_030730) do
-  create_table "testers", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2022_11_26_040240) do
+  create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.string "detail"
+    t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
