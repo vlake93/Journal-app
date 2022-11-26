c0347488a8ed21b970951c4fffb83544f72bd366
Vlake <apulivic@gmail.com>
2022-11-26 11:19:01 +0800

    added "tester" model for TDD dry run

---
+++ test/models/tester_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class TesterTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+
+  # test "should not save tester without name" do
+  #   tester = Tester.new
+  #   assert_not tester.save
+  # end
+  
+end
