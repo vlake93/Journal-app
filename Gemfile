db597e5cae07e8401059d5327b5edfc5e9a266f2
Vlake <apulivic@gmail.com>
2022-11-30 21:36:31 +0800

    added initializers

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ group :test do
 end
 
 gem "rspec-rails"
+
+gem "devise"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :category do
     resources :task
   end
+  resources :user
   
 
   # Defines the root path route ("/")
