a00c0e3e2d321d732ed54fa3600d4061806451ca
Vlake <apulivic@gmail.com>
2022-12-01 21:36:13 +0800

    created views for authentication and set sign in as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :category do
     resources :task
-  end
-  resources :user
-  
+  end  
 
   # Defines the root path route ("/")
-  root "category#index"
+  devise_for :users
+    devise_scope :user do
+  authenticated :user do
+    root :to => 'home#index', as: :authenticated_root
+  end
+  unauthenticated :user do
+    root :to => 'devise/sessions#new', as: :unauthenticated_root
+  end
+end
 end
